7599c6c44f6be835d102bb2ec35c0170757f407d
Martins Rudens <martins.rudens@gmail.com>
2017-10-24 17:38:07 +0300

    Compatible with python 3

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@
 from setuptools import setup
 
 setup(name='pypdf2xml',
-      version='0.3',
+      version='0.4',
       description='A reimplementation of pdftoxml in Python, using pdfMiner. Handles unicode characters better.',
       author=u'Gašper Žejn'.encode('utf-8'),
       author_email='zejn@owca.info',
       url='https://github.com/zejn/pypdf2xml',
       scripts=['pdf2xml', 'pdfxml2csv', 'headerfilter'],
       packages=['pypdf2xml'],
-      install_requires=['pdfminer==20140328', 'lxml'],
+      install_requires=['pdfminer.six', 'lxml'],
       classifiers=[
           'Development Status :: 4 - Beta',
           'Intended Audience :: Developers',
